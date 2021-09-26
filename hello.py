2208fded2cec3ee03c5040708181c0c236eb6d3c
Hiranya Maharaja <h.maharaja@mail.utoronto.ca>
2021-09-26 00:54:23 -0400

    reproduced example 4-7

--- hello.py
+++ hello.py
@@ -17,15 +17,16 @@ app.config['SECRET_KEY'] = 'gh678456'
 boostrap = Bootstrap(app)
 moment = Moment(app)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def index():    
-    name = None    
+@app.route('/', methods=['get', 'post'])
+def index():
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+             flash('looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
